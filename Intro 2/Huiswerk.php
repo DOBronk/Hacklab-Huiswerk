685b3e667aa0c84c158d12cb2d6692fd52510f43
Dennis Bronk <dbronk2@gmail.com>
2024-07-11 20:00:51 +0200

    English subs

--- Intro 2/Huiswerk.php
+++ Intro 2/Huiswerk.php
@@ -1,37 +1,38 @@
 <?PHP
 
-// Maximale getal ingesteld door de gebruiker
+// Maximum number
 $maxNumber = 0;
 
 while (!$maxNumber) {
-    /* Loopje tot we een juist antwoord hebben */
+    /* Loop until we have the right answer */
     $input = readline("Geef een maximaal getal boven 0: ");
     if (is_numeric($input) && $input >= 1) {
-        // Stel het maximale getal in, cast naar int om eventuele float van floor te krijgen
+        // Set the maximum user input value as int
         $maxNumber = (int) $input;
     } else {
+        // The user input was invalid
         echo "Het ingevoerde was geen getal of niet hoger dan 0!\n";
     }
 }
-
-// Bepaal het willekeurige getal tussen 0 en $maxNumber
+// Decide the random number between 0 and maximum given number
 $randNumber = rand(0, $maxNumber);
 
 for ($i = 0; $i < 10; $i++) {
-    /* For loopje voor 10 pogingen */
+    /* Loop 10 times */
     $guessedNumber = readline("Geef een getal om te gokken: ");
     if (is_numeric($guessedNumber) && (int) $guessedNumber == $randNumber) {
         // Winner!
         echo "Goed gegokt! Getal was $randNumber\n";
         break;
     } elseif (!is_numeric($guessedNumber) || $guessedNumber < 0) {
-        // Invoer klopt niet
+        // The user input was invalid
         echo "Het ingevoerde was geen getal of lager dan 0\n";
     } elseif ($i == 9) {
-        // Loop loopt tegen zijn einde, helaas pindakaas
+        // End of loop, you lost
         echo "Helaas je hebt het getal niet kunnen raden. Het getal was $randNumber\n";
     } else {
-        echo "Je hebt fout gegokt, nog " . 9 - $i . " pogingen te gaan!\n";
+        // User guessed wrong, show the attemps that the user has left
+        echo "Je hebt fout gegokt, nog " . (9 - $i) . " pogingen te gaan!\n";
     }
 }
 
