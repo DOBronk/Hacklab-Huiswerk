da6ba9e784fbd53157cb37b895a8e69c14173105
Dennis Bronk <dbronk2@gmail.com>
2024-07-17 20:23:57 +0200

    Cleaner and more dynamic code with added error checking.

--- Puzzeltjes/PHP/fragmented_pass.php
+++ Puzzeltjes/PHP/fragmented_pass.php
@@ -1,19 +1,28 @@
 <?php
+// Holds the total number of chunks as specified in the files
+$length = 0;
 
 /**
- * Opens the file and filters out the data chunks and their positions 
- * @param string $file   Path to file
+ * Opens the file and filters out the data chunks and their positions
+ * @param string $file Path to file
  * @return array  Returns associative array with all found password chunks using their position as key
  */
-function handleTextFile(string $file): array
+function parseTextFile(string $file): array
 {
+    global $length;
+
     $file_data = file_get_contents($file);
     $lines = explode("\n", $file_data);
     $pieces = [];
 
     foreach ($lines as $line) {
+        // Check to see if the line contains a character that is only used in password chunks
         if (str_contains($line, "[")) {
             $begin = strstr($line, "[");
+            // If we haven't set the total length yet, now is the time to do so.
+            if (!$length) {
+                $length = (int) strstr(explode('/', $begin)[1], ']', true);
+            }
             $pos = substr(explode('/', $begin)[0], 1);
             $pieces[$pos] = explode("]", $line)[1];
         }
@@ -21,17 +30,40 @@ function handleTextFile(string $file): array
 
     return $pieces;
 }
+/**
+ * Construct the password bij creating and merging arrays with chunks together and looping through them by number
+ * @param string[] $files All files to be parsed in string format
+ * @return string Constructed password
+ */
+function constructPassword(string ...$files): string
+{
+    global $length;
 
-$final = handleTextFile("Data/FitnessPlan.txt");
-$final += handleTextFile("Data/GroceryList.txt");
-$final += handleTextFile("Data/RecipeCollection.txt");
-$final += handleTextFile("Data/TravelItinerary.txt");
-$final += handleTextFile("Data/WorkSchedule.txt");
+    $final = [];
+    $result = '';
+    // Parse each text file and then merge it with final array
+    foreach ($files as $file) {
+        $final += parseTextFile($file);
+    }
+    // Return an error if the number of chunks don't match up with what's expected
+    if (count($final) != $length) {
+        return "ERROR: There should be $length chunks available, but " . count($final) .
+            " chunks were found. Did you include all files in the arguments?";
+    }
+    // Construct the final string by itterating from 1 to total chunks
+    for ($i = 1; $i < $length + 1; $i++) {
+        $result .= $final[$i];
+    }
 
-$result = '';
-
-for ($i = 1; $i < 21; $i++) {
-    $result .= $final[$i];
+    return $result;
 }
 
-echo $result . PHP_EOL;
+// Print the result
+echo constructPassword(
+    "Data/FitnessPlan.txt",
+    "Data/GroceryList.txt",
+    "Data/RecipeCollection.txt",
+    "Data/TravelItinerary.txt",
+    "Data/WorkSchedule.txt"
+);
+
