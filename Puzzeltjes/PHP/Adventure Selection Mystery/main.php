add85375ed79015a4f79872293dcc0554f5c74b8
Dennis Bronk <dbronk2@gmail.com>
2024-07-18 15:10:05 +0200

    Unraveling the Adventure Selection Mystery puzzle

---
+++ Puzzeltjes/PHP/Adventure Selection Mystery/main.php
@@ -0,0 +1,19 @@
+<?php
+// Load the filecontents in string
+$file = file_get_contents("games_list.json");
+// Decode JSON to Array
+$data = json_decode($file, true);
+// Result string
+$result = '';
+
+foreach ($data as $value) {
+    // Add # to result if matches requirements or . when unsuitable
+    $result .= ($value[1] <= 7 && $value[2] == "Adventure") ? '#' : '.';
+    if (strlen($result) == 18) {
+        // Result line reached 18 characters, time to spit out some data and add a newline
+        echo $result . PHP_EOL;
+        // Clear the output
+        $result = '';
+    }
+}
+
